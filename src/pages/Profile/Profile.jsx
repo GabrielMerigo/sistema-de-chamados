99f0cb5bcdf2015f1c46d46ec3c92cef6214c190
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-28 22:37:48 -0300

    funcionalidade de alteração de foto

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,19 @@
-import '../Profile/profile.css'
+import '../Profile/profile.css';
+import { useState, useContext } from 'react';
 import Header from '../../components/Header/Header'
 import Title from '../../components/Title/Title.jsx'
-import { FiSettings } from 'react-icons/fi'
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import { AuthContext } from '../../contexts/auth';
+
+import avatar from '../../assets/avatar.png'
 
 export default function Profile() {
+  const { user, signOut } = useContext(AuthContext)
+
+  const [nome, setNome] = useState(user && user.nome)
+  const [email, setEmail] = useState(user && user.email)
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatar)
+  
   return (
     <div>
       <Header />
@@ -11,6 +21,36 @@ export default function Profile() {
         <Title name="Meu Perfil">
           <FiSettings size={25} />
         </Title>
+        <div className="container">
+          <form className="form-profile" >
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#FFF" size={25}/>
+              </span>
+
+              <input type="file" accept="image/*" /><br />
+              {avatarUrl === null ? 
+                <img src={avatar} width="250" alt="foto de perfil do usuário" />
+                : <img src={avatarUrl} width="250" alt="foto de perfil do usuário" />
+              }
+            </label>
+
+            <label>Nome:</label>
+            <input type="text" value={nome} onChange={e => setNome(e.target.value) } />
+
+            <label>Email</label>
+            <input type="text" value={email} disabled={true}/>
+
+            <button onSubmit={true}>Salvar</button>
+
+          </form>
+        </div>
+
+        <div className="container">
+          <button className="logout-btn" onClick={() => signOut()} >
+            Sair
+          </button>
+        </div>
       </div>
     </div>
   )

8bcb81b582fccd69019dcda9849225f240d82e9b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-06-28 22:53:21 -0300

    quase

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,15 +4,38 @@ import Header from '../../components/Header/Header'
 import Title from '../../components/Title/Title.jsx'
 import { FiSettings, FiUpload } from 'react-icons/fi'
 import { AuthContext } from '../../contexts/auth';
+import firebase from '../../services/firebaseConnection';
 
 import avatar from '../../assets/avatar.png'
 
 export default function Profile() {
-  const { user, signOut } = useContext(AuthContext)
+  const { user, signOut, setUser, storageUser } = useContext(AuthContext)
 
   const [nome, setNome] = useState(user && user.nome)
   const [email, setEmail] = useState(user && user.email)
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatar)
+  const [imageAvatar, setImageAvatar] = useState(null)
+
+  async function handleSave(e){
+    e.preventDefault()
+
+    if(imageAvatar === null && nome !== ''){
+      await firebase.firestore().collection('users')
+        .doc(user.uid)
+        .update({
+          nome: nome
+        })
+      .then(() => {
+        let data = {
+          ...user,
+          nome: nome
+        }
+
+        setUser(data)
+        storageUser(data)
+      })
+    }
+  }
   
   return (
     <div>
@@ -22,7 +45,7 @@ export default function Profile() {
           <FiSettings size={25} />
         </Title>
         <div className="container">
-          <form className="form-profile" >
+          <form className="form-profile" onSubmit={handleSave}>
             <label className="label-avatar">
               <span>
                 <FiUpload color="#FFF" size={25}/>
